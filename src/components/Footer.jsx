18db49e7468bcb28796969581da18d56e437be24
MrPhors <142238273+MrPhors@users.noreply.github.com>
2025-10-31 23:40:50 +0700

    last commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
      <footer className="bg-[#0a192f] text-center py-4 border-t border-gray-700">
         <p className="text-gray-400 text-sm">
           © {new Date().getFullYear()} Create by{" "}
-          <span className="text-teal-400 font-medium">Mrr. Phors</span>. All rights reserved.
+          <span className="text-teal-400 font-medium">Mrr. Phors - Uzita</span>. All rights reserved.
         </p>
       </footer>
   )
